5e8e6a1d0f51d9198dfce4b495899b2cbf919ac2
Emma <lyynk@icloud.com>
2026-06-03 10:28:05 -0400

    bump: version 1.0.1 for the new release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Fae_Alchemy'
 description 'A lightweight, framework-agnostic bridge for custom scripts'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
